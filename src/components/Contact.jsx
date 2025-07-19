c8e7150d00be2dae3588f7a9cd7891c49f4b52d9
ChetanThorat18 <chetanthorat146@gmail.com>
2025-07-20 00:08:56 +0530

    Updated Contact Section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -29,8 +29,8 @@ const Contact = () => {
     //click on create a new template then click on save.
     emailjs
       .send(
-        'serviceID', // paste your ServiceID here (you'll get one when your service is created).
-        'templateID', // paste your TemplateID here (you'll find it under email templates).
+        import.meta.env.VITE_SERVICE_ID,// paste your ServiceID here (you'll get one when your service is created).
+        import.meta.env.VITE_TEMPLATE_ID, // paste your TemplateID here (you'll find it under email templates).
         {
           from_name: form.name,
           to_name: 'YourName', // put your name here.
@@ -38,7 +38,7 @@ const Contact = () => {
           to_email: 'youremail@gmail.com', //put your email here.
           message: form.message,
         },
-        'yourpublickey' //paste your Public Key here. You'll get it in your profile section.
+        import.meta.env.VITE_PUBLIC_KEY, // Public Key from .env
       )
       .then(
         () => {
